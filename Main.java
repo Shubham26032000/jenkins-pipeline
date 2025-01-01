0c24a115703ac2b8063657edd82588c4890de6a4
Shubham Vast <shubhamvast5@gmail.com>
2025-01-01 17:54:37 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Working with Jenkins pipelines");
+        System.out.println("Yooo hoooo....!");
+    }
+}
